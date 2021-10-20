43080baa5294f3a6b706235f38521603ec2cecc2
Hectorv72 <hectorvaldezfsa13@gmail.com>
2021-10-20 16:53:37 -0300

    20/10/21 modelo profesionales

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -2,7 +2,6 @@ const { model, Schema } = require("mongoose");
 
 const UserSchema = new Schema({
   email: { type: String, required: true },
-  username: { type: String, required: true },
   password: { type: String, required: true },
   rol: { type: String, required: true },
   active: { type: Boolean, default: true },
