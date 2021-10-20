574bf3a367eb3c6d04830c98372a6dc31381d708
Hectorv72 <hectorvaldezfsa13@gmail.com>
2021-10-20 16:24:13 -0300

    first commit

---
+++ src/models/user.models.js
@@ -0,0 +1,11 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  rol: { type: String, required: true },
+  active: { type: Boolean, default: true },
+});
+
+module.exports = model("User", UserSchema);
